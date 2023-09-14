a83c6dd461589ca9a511ed73291823525763cbc7
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-09-14 23:08:19 +0300

    benefits-section

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 import './home/hero-section/hero-section.scss';
 import './home/hero-section/hero-section.ts';
 import './home/about-section/about-section.scss';
+import './home/benefits-section/benefits-section.scss';
