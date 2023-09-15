8c26859a41538cb40db260a85d08c89823236742
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-09-15 22:13:38 +0300

    contacts-section

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ import './home/hero-section/hero-section.scss';
 import './home/hero-section/hero-section.ts';
 import './home/about-section/about-section.scss';
 import './home/benefits-section/benefits-section.scss';
+import './home/contacts-section/contacts-section.scss';
