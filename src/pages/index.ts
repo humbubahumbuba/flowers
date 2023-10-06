52e397d1a76c9b86de91c73041d12bf3af95af63
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-10-06 23:59:01 +0300

    review_section

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,4 +4,5 @@ import './home/about-section/about-section.scss';
 import './home/benefits-section/benefits-section.scss';
 import './home/contacts-section/contacts-section.scss';
 import './home/service-section/service-section.scss';
+import './home/reviews-section/reviews-section.scss';
 import './home/footer/footer.scss';
