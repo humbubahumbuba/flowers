d6274115cd61a19b5c789b8891737ee5d29415a4
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-11-05 23:42:32 +0200

    swiper

--- src/pages/home/reviews-section/ReviewsSection.tsx
+++ src/pages/home/reviews-section/ReviewsSection.tsx
@@ -1,6 +1,13 @@
 import { FC } from 'react';
 import './reviews-section';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, EffectCoverflow } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import './swiper.scss';
+
 
 
 export const ReviewsSection: FC = () => {
@@ -14,40 +21,55 @@ export const ReviewsSection: FC = () => {
         <h2 className="reviews-section__header">Our Clients say</h2>
       </div>
 
-      <div className="swiper j-swiper">
-        <div className="swiper-wrapper">
-          <div className="swiper-slide">
-            <div className="reviews-section__slide">
+     <Swiper
+            modules={[Navigation, Pagination, EffectCoverflow]}
+            pagination={{
+                el: '.swiper-pagination',
+                clickable: true
+            }}
+            navigation={{
+                nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+            }}
+           loop={true}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+            <SwiperSlide>
+                <div className="reviews-section__slide">
               <p className="reviews-section__slide-text">
                 “Ordered flowers online and they were the best bouquet!
                 Impressed everyone around. Highly recommend this flower shop!”
               </p>
               <p className="reviews-section__slide-reviewer">– Ronald Richards</p>
             </div>
-          </div>
-          <div className="swiper-slide">
-            <div className="reviews-section__slide">
+      </SwiperSlide>
+            <SwiperSlide>
+                <div className="reviews-section__slide">
               <p className="reviews-section__slide-text">
                 “Ordered flowers online and they were the best bouquet!
                 Impressed everyone around. Highly recommend this flower shop!”
               </p>
               <p className="reviews-section__slide-reviewer">– Ronald Richards</p>
             </div>
-          </div>
-          <div className="swiper-slide">
-            <div className="reviews-section__slide">
+      </SwiperSlide>
+            <SwiperSlide>
+                <div className="reviews-section__slide">
               <p className="reviews-section__slide-text">
                 “Ordered flowers online and they were the best bouquet!
                 Impressed everyone around. Highly recommend this flower shop!”
               </p>
               <p className="reviews-section__slide-reviewer">– Ronald Richards</p>
             </div>
-          </div>
-        </div>
-        <div className="swiper-pagination"></div>
+      </SwiperSlide>
+      <div className="swiper-pagination"></div>
         <div className="swiper-button-prev"></div>
         <div className="swiper-button-next"></div>
-      </div>
+        
+            
+     
+        </Swiper>
       <button
         type="button"
         className="button button--secondary reviews-section__button uppercase"
